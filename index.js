9e3c25bf26872c902f9b9d97f0744474f13e2006
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-24 16:46:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function breakOut(array, changeValue, stopValue) {
    for(let i = 0; i < array.length; i++) {
        if(array[i] == skipValue) {
          continue
-       } 
+       }
       array[i] = changeValue 
      }
  }
